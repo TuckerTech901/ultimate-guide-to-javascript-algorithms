0a86082be656153cd755a628fa55f2e191d02448
TuckerTech901 <TuckerTech901@gmail.com>
2022-10-12 22:13:24 -0400

    Update index-START.js
    Forked and updated repository

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -4,10 +4,62 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
+//** split-reverse-join method */
+// function reverseString(text) {
+//     return text.split("").reverse().join("") 
+// }
+
+//**spread operator example */
+
+function reverseString(text){
+    return [...text].reverse().join('')
+}
+
+// **for loop example** //
+
 
 function reverseString(text) {
-    // Code goes here
+    let result = ""
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        result += text[i]
+    }
+
+    return result
 }
+// **The for loop is looping in reverse this the i>= 0 and i--**
+
+//** the for of way */
+
+function reverseString (text){
+    let result = "";
+     
+    for (let char of text){
+        result = char + result
+    }
+    return result;
+}
+
+// ** the recursive way */
+
+function reverseString (text){
+    if (text ===""){
+        return ""
+    }else {
+        return reverseString(text.substr(1)) + text [0]
+    }
+}
+
+//**reduce method */
+// function reverseString(text){
+//   return text.split("").reduce((acc, char)) => + acc, '')
+// }
+
+ /* with spread operator*/
+
+ function reverseString(text){
+  return [...text].reduce((acc, char) => char + acc, '')
+ }
 
 
 
